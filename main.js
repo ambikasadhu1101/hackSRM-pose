fe04d955796c7f7da41d4ca63a50bf167ff804ed
ambika <ambikasadhu1101@gmail.com>
2020-11-22 02:00:11 +0530

    frontpage changed

--- main.js
+++ main.js
@@ -1,6 +1,41 @@
-document
-  .querySelector(".testimonial-container")
-  .addEventListener("click", slideTestimonial);
+// $(document).ready(function(){
+//   $(window).scroll(function(){
+//       // sticky navbar on scroll script
+//       if(this.scrollY > 20){
+//           $('.main-nav').addClass("sticky");
+//       }else{
+//           $('.main-nav').removeClass("sticky");
+//       }
+      
+//       // scroll-up button show/hide script
+//       if(this.scrollY > 500){
+//           $('.scroll-up-btn').addClass("show");
+//       }else{
+//           $('.scroll-up-btn').removeClass("show");
+//       }
+//   });
+// });
+// document.querySelector(document).ready(function(){
+//   document.querySelector(window).scroll(function(){
+//       // sticky navbar on scroll script
+//       if(this.scrollY > 20){
+//           document.querySelector('.main-nav').classList.add("sticky");
+//       }else{
+//           document.querySelector('.main-nav').removeClass("sticky");
+//       }
+      
+//       // scroll-up button show/hide script
+//       if(this.scrollY > 500){
+//           document.querySelector('.scroll-up-btn').classList.add("show");
+//       }else{
+//           document.querySelector('.scroll-up-btn').removeClass("show");
+//       }
+//   });
+// // });
+AOS.init({
+  duration: 1200,
+})
+document.querySelector(".testimonial-container").addEventListener("click", slideTestimonial);
 
 let user1 = document.getElementById("user-1");
 let user2 = document.getElementById("user-2");
@@ -76,3 +111,46 @@ function slideTestimonial(e) {
 function squatsClick(){
   window.open("index.html","_self")
 }
+"use strict";
+const sleep = delay => new Promise(resolve => setTimeout(resolve, delay))
+const humanize = speed => Math.round(Math.random() * (100 - 30)) + speed
+async function typed(opts) {
+	let {
+		strings, 
+		elm, 
+		backDelay = 500,
+		eraseSpeed = 10,
+		typeSpeed = 30,	
+		loop = false
+	} = opts
+	
+	do {
+		let last = strings.length
+		let index = 0
+
+		for (let string of strings) {
+			index++
+
+			for (let character of string) {
+				await sleep(humanize(typeSpeed))
+				elm.innerText += character
+			}
+
+			if (!loop && last == index) return
+
+			await sleep(backDelay)
+
+			while (string) {
+				await sleep(humanize(eraseSpeed))
+				elm.innerText = string = string.slice(0, -1)
+			}
+		}
+	} while (loop)
+}
+
+typed({
+	elm: document.querySelector(".main-heading-focus"),
+	strings: ['Smart.', 'Indoor.', 'Fitness.','Trainer.'],
+	// loop: false <-- default
+}).then(() => console.log('done with animation'))
+
